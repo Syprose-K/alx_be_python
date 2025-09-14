37261af23e5b17d8bf8e2973eaa745d323c9b777
syprose-k <kichesyprose@gmail.com>
2025-09-14 14:49:46 +0300

    I added a folder and file the code inside the file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+number1 = 10
+number2 = 5
+sum = number1 + number2
+print ("Addition of " + str(number1) + " and " + str(number2) + " is " + str(sum))
+
+difference = number1 - number2
+print ("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(difference))
+
+product = number1 * number2
+print ("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(product))
+
